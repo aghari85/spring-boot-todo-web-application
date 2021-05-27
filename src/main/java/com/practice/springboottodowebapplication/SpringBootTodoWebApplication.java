e2d66a48edd23b5ff43c409d8d5071515fd89bcf
aghari85 <ghari85@gmail.com>
2021-05-27 13:47:08 +0530

    initial commit

---
+++ src/main/java/com/practice/springboottodowebapplication/SpringBootTodoWebApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springboottodowebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTodoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/springboottodowebapplication/SpringBootTodoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboottodowebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
