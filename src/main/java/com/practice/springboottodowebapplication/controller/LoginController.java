d760c968deaf5c6a9d828dad8d8ecd1131404a0b
aghari85 <ghari85@gmail.com>
2021-05-28 12:58:44 +0530

    request param. shows name in jsp page.

--- src/main/java/com/practice/springboottodowebapplication/controller/LoginController.java
+++ src/main/java/com/practice/springboottodowebapplication/controller/LoginController.java
@@ -1,14 +1,18 @@
 package com.practice.springboottodowebapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    public String loginMessage(){
+    public String loginMessage(@RequestParam String name, ModelMap modelMap){
+        modelMap.put("name", name);
         return "login";
     }
 }
